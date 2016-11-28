a839bbba4eeb1bb660f598ea762b8325eb5c9851
chparker <rekrap123@hotmail.com>
2016-11-28 16:54:12 +0100

    34 - 35

---
+++ ex34.py
@@ -0,0 +1,9 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+"""
+The first animal is at 0 and is a bear
+The second animal is at 1 and is a python
+The third animal is at 2 and is a peacock
+The fourth animal is at 3 and is a kanagroo
+The fifth animal is at 4 and is a whale
+The 6th animal is at 5 and is a platypus
