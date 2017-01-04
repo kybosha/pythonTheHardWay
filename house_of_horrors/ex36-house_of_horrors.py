735ac8c62546dec98dee494aed96daaa79fa18e3
Chris Parker <chparker@atlassian.com>
2017-01-04 20:56:46 +0100

    house all text for game in new file

--- house_of_horrors/ex36-house_of_horrors.py
+++ house_of_horrors/ex36-house_of_horrors.py
@@ -1,13 +1,9 @@
-# House of Horrors Python Game - aim of the game is to escape the house of horros alive, the only method of escape is by driving away in the car parked out front whose keys you need to find. 
+# House of Horrors Python Game - ai of the game is to escape the house of horros alive, the only method of escape is by driving away in the car parked out front whose keys you need to find. 
 # Follow the instructions and make your descions wisely.
 import random
 import os,sys
-import text
-import time
-from random import randrange
 
-print opening_line
-#print "Let's start with a few questions."
+print "Let's start with a few questions."
 
 shower_leak = False
 
@@ -65,22 +61,26 @@ def clear_screen():  #Simple function that clears the screen
 
 # The opening sequence
 def start():
-	for i in start_text.split():
-		sys.stdout.write("{} ".format(i))
-		sys.stdout.flush()
-
-		seconds = ".6" + str(randrange(1,5,2))
-		seconds = float(seconds)
-		time.sleep(seconds)
-
-	#print start_text
+	print """A test, a test for the soul while the mind is occupied. Save your soul and your mind may just flicker again, consciousness is a cruel concept. You need a foundation, 
+	a corner stone in which to base your percpetions on, don't keep your eyeys shut for too long, when you open them things may seem too real. I understand, you're confused. That's 
+	perfectly reasonable, but you you need to take action now if you wish to become lucid again. Now wake up, but not completely, you can only save yourself if your self thinks it's awake.
+	\nI can be of some assistance later on, things may become clearer, but for now wake up!\n"""
 
 	print "Wake up!\n" * 5
 	
 	wake()
 
 def wake():
-	print wake_text
+	print """As his eyes creaked opened he became aware of the throbbing pain that was present in the back of his head. Reaching back he brushed his fingers across an an egg sized 
+	lump that plateaued out from his head, it felt as though it was buffering against his brain as the vibrations could be felt throughout his body. As his surroundings started to 
+	become clear he realised he was lying on his side surrounded by a quilt of fallen autumn leaves, as he shifted his weight they crunched underneath him clunging to his clothes 
+	while he pushed himself up. Craning his neck he could see he was surrounded by woodland, it was extremely dark though 
+	so he could only see the first row of trees. The only light source available was the dull glow of the moon, it excentuated the silhouettes of the objects that surrounded him but 
+	didn't reveal any more detail. There was a break in the trees far off to his right and stationed in front of it was a small car, he couldn't make out much more than that due to 
+	the low visibility.\n Suddenly there was a bang behind him, he jolted round he found himself outside the front of an old wooden house, it was a 2 storey cabin. Agahst he scanned 
+	the front of the house erratically searching for the source of the noise. All was quiet though. Too quiet he thought, there were no animal noises, no trees rustling in the wind. 
+	In fact there was no wind at all. The back of his head began to throb again causing him to double over as he clutched at the swollen lump pertruding from it. ****WRITE MORE***
+	"""
 	print "Panic rose up from his gut filling his mind with anxiety, he had a split decision to make, run or enter the house?"
 	enter_run = raw_input("> ")
 	if "run" in enter_run:
@@ -105,7 +105,8 @@ def run():
 		run()
 
 def enter_house():
-	print enter_house_text
+	print """House is entered looks around a bit blah blah blah, on the floor he finds a note written on a post-it note. Not sure where the basement is just yet.  
+	Note reads that the car keys have been left in the safe in the basement, combination is 17XX - the last 2 digits are unintellgible.\n \"I need to find those last 2 digits he thought.\""""
 	downstairs_upstairs()
 
 
@@ -136,26 +137,27 @@ def downstairs_upstairs():
 		downstairs_upstairs()
 
 def up():
-	print upstairs_text
+	print """He walks up stairs and finds a long straight corridor with 3 rooms, 2 on the left 1 on the right. blah blah blah - does he enter one or head back downstairs?"""
 	upstairs_room_choice = raw_input("> ")
 	if "1" in upstairs_room_choice:
-		print room1_entry_text
+		print "Blah Blah blah - He enters the first room"
 		room1()
 	elif "2" in upstairs_room_choice:
-		print room2_entry_text
+		print "Blah Blah blah - He enters the second room"
 		room2()
 	elif "3" in upstairs_room_choice:
-		print room3_entry_text
+		print "Blah Blah blah - He enters the third room"
 		room3()
 	elif "down" in upstairs_room_choice:
 		downstairs()
 
 
 def room1():
-	print room1_initial_text
+	print """Enters room 1 - turns out to be a bedroom, bedframe with bedside table, wardrobe. What should he inspect?
+	"""
 	room1_inspect = raw_input("> ")
 	if "bed" in room1_inspect:
-		print room1_bed_text
+		print "Shit about what the bed looks like\n"
 		print "should he check the wardrobe as well or go back to the landing?"
 		room1_inspect2 = raw_input("> ")
 		if "wardrobe" in room1_inspect2:
@@ -202,7 +204,9 @@ def room1():
 
 	
 def room2():
-	print room2_initial_text
+	print """Enters room 2 - it's the bathroom, blah blah blah. There's a shower, a broken toilet bowl and not much else. The light is flickering, the shower is dripping heavily
+	turn the shower handle to stop the dripping? 
+"""	
 	shower_turn = raw_input("> ")
 	if "turn" in shower_turn:
 		if "Coat Hanger" in items:
@@ -223,7 +227,7 @@ def room2():
 			upstairs_hallway()
 
 	else:
-		print room2_no_fix_shower
+		print "The dripping wasn't so bad after all blah blah blah, back to the landing"
 		shower_leak is False
 # REMOVE AFTER TESTING
 		print items
@@ -231,28 +235,36 @@ def room2():
 
 
 def room3():
-	print room3_initial_text
+	print """ Third room is a study/office, it is baren except for a desk in front of a large bare window looking out onto the front yard of the house, 
+	the only light entering the room dully creeps in from the distant moon - blah blah blah. Walking over to the desk what should be explored? 
+	Look out the window, check out desk?
+
+	"""
 	window_desk = raw_input("> ")
 	if "window" in window_desk:
-		print room3_window_text
+		print """He looks out the window out across the yard...blah blah blah, he can see the outline of the car out towards the tree line reminding him
+		of what his goals are. He needs to get into that basement, where's the key though? And what's the rest of the combination for the safe? Should he check out
+		the desk now or head back out the room?"""
 		desk_leave = raw_input("> ")
 		if "leave" in desk_leave:
 			print "Heads back out to the landing."
 			upstairs_hallway()
 		elif "desk" in desk_leave:
-			print room3_window_desk_text
+			print """Turning his gaze away from the window he looks over the desk, pretty dark so he's not able to see much, tries to open the drawers he finds
+			but they are empty. There's an old lamp sitting on the top of the desk, it may still work should he turn it on?"""
 			room3_lamp()
 		else:
 			print "That's not an option."
 			room3()
 	elif "desk" in window_desk:
-		print room3_desk_text
+		print "Shit about the desk, ask whether to look out the window or leave."
 		window_leave = raw_input("> ")
 		if "window" in window_leave:
-			print room3_desk_window_text
+			print "He looks out the window out across the yard...blah blah blah then turns and leaves the room."
 			upstairs_hallway()
 		else:
-			print room3_whisper_text
+			print """He leaves the room, but hears a rustling from the corner of the room making he swing round in fright. There's nothing there so he leaves again, he then 
+			hears a whisper \"LAMP\""""
 			upstairs_hallway()
 
 
@@ -264,7 +276,11 @@ def room3_lamp():
 		lamp_on = False
 
 	if lamp_on is True:
-		print room3_lamp_text
+		print """He turns on the lamp, it fizzes and flickers as it attmepts to illuminate the desk, after a few seconds of flickering it goes full beam blinding him, he shields
+		his eyes momentarily. Slowly his eyes begin to adjust to the new lighting level and drops his arm from his eyes, he's looking at the window now but the new ligting means
+		it's reflected and so it is himself and the room that is looking back at him. He notices a post-it notepad in the refelction of the window resting on the table now. Picking it up 
+		he can see there are imprints on it...it must be the pad that the note he found was written on. Holding it under the lamp he can see it clearly, scanning it quickly he finds the the full
+		code - 1792!"""
 		items.append(code)
 		print items
 		print "Back to the landing."
@@ -282,7 +298,7 @@ def upstairs_hallway():
  	"""
 	upstairs_hallway_choice = raw_input("> ")
 	if "down" in upstairs_hallway_choice:
-		print upstairs_hallway_choice_down
+		print "He heads back down the stairs blah blah blah"
 		downstairs()
 	elif "1" in upstairs_hallway_choice:
 		room1()
@@ -295,7 +311,8 @@ def upstairs_hallway():
 		upstairs_hallway()
 
 def downstairs():
-	print downstairs_initial_text
+	print """On the ground floor he blah blah blah blah - looks around a little there are 4 rooms, more description about what he sees, he needs to pick a room - A, 
+	B, C, or D Or should he go upstairs? Alternatively head back outside?"""
 	downstairs_room_choice = raw_input("> ")
 	if "a" in downstairs_room_choice:
 		print "He enters room A"
@@ -324,16 +341,40 @@ def rooma():
 	while "Knife" in items:
 					print "The body of the demon lies in its own filth. You head back out to the landing."
 					return downstairs()
-	print rooma_intial_text
+	print """Enters Room A blah blah blah - it's the kitchen, looks around blah blah blah blah. Description of the kitchen and it's rustic demeanor. Get to the point 
+	where something enters and he has to reach for a knife but drops it down a crack and tries to reach for it in order to defend himself. The crack is slim, """
 	if state == "Overweight" or state == "Obese":
-		print rooma_too_fat_text
+		print "You too fat! You dead! blah blah blah monster rips you to pieces"
 		dead("Monster tears you a new one.")
 	else:
 		print "you manage to pull the knife out and begin the epic battle with the demon\n He's terrifying!"
-		print demon_text
+		print """
+		                    ,-.                               
+       ___,---.__          /'|`\          __,---,___          
+    ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-.       
+  ,'        |           ~'\     /`~           |        `.      
+ /      ___//              `. ,'          ,  , \___      \    
+|    ,-'   `-.__   _         |        ,    __,-'   `-.    |    
+|   /          /\_  `   .    |    ,      _/\          \   |   
+\  |           \ \`-.___ \   |   / ___,-'/ /           |  /  
+ \  \           | `._   `\\  |  //'   _,' |           /  /      
+  `-.\         /'  _ `---'' , . ``---' _  `\         /,-'     
+     ``       /     \    ,='/ \`=.    /     \       ''          
+             |__   /|\_,--.,-.--,--._/|\   __|                  
+             /  `./  \\`\ |  |  | /,//' \,'  \                  
+            /   /     ||--+--|--+-/-|     \   \                 
+           |   |     /'\_\_\ | /_/_/`\     |   |                
+            \   \__, \_     `~'     _/ .__/   /            
+             `-._,-'   `-._______,-'   `-._,-'
+
+
+		"""
 		battle()
 
 def battle():
+	#while "Knife" in items:
+	#				print "The body of the demon lies in its own filth. You head back out to the landing."
+	#				return downstairs()
 	print "The demon lunges at you, lunging forward with the knife you aim for it's chest"
 	your_num = random.randrange(1, 10)
 	# for testing purposes
@@ -425,7 +466,16 @@ def explore_basement():
 	print """Wondering around the basement blah blah blah, you knock over a pile of boxes - a creepy old man emerges from the shadows. You shite yourslef, or atleast it feels like you did. The old man grins a
 	torid smile and then tells you he has a riddle for you. If you answer correctly he will gift you something, if you answer incorrectly something terrible will happen. One word is requied for te answer.\n"""
 	
-	print poem
+	print """
+	A flame bereft,
+		of control.
+	A bottomless depth,
+		without soul.
+	A hellish pit of,
+		sadistic comedy.
+	An inaugural piece of,
+		divine trinity.\n
+	"""
 	print "With a death like expression he demands an answer."
 	riddle_answer = raw_input("> ")
 	if "inferno" in riddle_answer:
@@ -434,7 +484,60 @@ def explore_basement():
 	else:
 		print "The old man's smile broadens and he lets out an evil cackle, 'times up he screams!'\n"	
 
-		print skull
+		print """
+		                          ...----....
+	                         ..-:"''         ''"-..
+	                      .-'                      '-.
+	                    .'              .     .       '.
+	                  .'   .          .    .      .    .''.
+	                .'  .    .       .   .   .     .   . ..:.
+	              .' .   . .  .       .   .   ..  .   . ....::.
+	             ..   .   .      .  .    .     .  ..  . ....:IA.
+	            .:  .   .    .    .  .  .    .. .  .. .. ....:IA.
+	           .: .   .   ..   .    .     . . .. . ... ....:.:VHA.
+	           '..  .  .. .   .       .  . .. . .. . .....:.::IHHB.
+	          .:. .  . .  . .   .  .  . . . ...:.:... .......:HIHMM.
+	         .:.... .   . ."::"'.. .   .  . .:.:.:II;,. .. ..:IHIMMA
+	         ':.:..  ..::IHHHHHI::. . .  ...:.::::.,,,. . ....VIMMHM
+	        .:::I. .AHHHHHHHHHHAI::. .:...,:IIHHHHHHMMMHHL:. . VMMMM
+	       .:.:V.:IVHHHHHHHMHMHHH::..:" .:HIHHHHHHHHHHHHHMHHA. .VMMM.
+	       :..V.:IVHHHHHMMHHHHHHHB... . .:VPHHMHHHMMHHHHHHHHHAI.:VMMI
+	       ::V..:VIHHHHHHMMMHHHHHH. .   .I":IIMHHMMHHHHHHHHHHHAPI:WMM
+	       ::". .:.HHHHHHHHMMHHHHHI.  . .:..I:MHMMHHHHHHHHHMHV:':H:WM
+	       :: . :.::IIHHHHHHMMHHHHV  .ABA.:.:IMHMHMMMHMHHHHV:'. .IHWW
+	       '.  ..:..:.:IHHHHHMMHV" .AVMHMA.:.'VHMMMMHHHHHV:' .  :IHWV
+	        :.  .:...:".:.:TPP"   .AVMMHMMA.:. "VMMHHHP.:... .. :IVAI
+	       .:.   '... .:"'   .   ..HMMMHMMMA::. ."VHHI:::....  .:IHW'
+	       ...  .  . ..:IIPPIH: ..HMMMI.MMMV:I:.  .:ILLH:.. ...:I:IM
+	     : .   .'"' .:.V". .. .  :HMMM:IMMMI::I. ..:HHIIPPHI::'.P:HM.
+	     :.  .  .  .. ..:.. .    :AMMM IMMMM..:...:IV":T::I::.".:IHIMA
+	     'V:.. .. . .. .  .  .   'VMMV..VMMV :....:V:.:..:....::IHHHMH
+	       "IHH:.II:.. .:. .  . . . " :HB"" . . ..PI:.::.:::..:IHHMMV"
+	        :IP""HHII:.  .  .    . . .'V:. . . ..:IH:.:.::IHIHHMMMMM"
+	        :V:. VIMA:I..  .     .  . .. . .  .:.I:I:..:IHHHHMMHHMMM
+	        :"VI:.VWMA::. .:      .   .. .:. ..:.I::.:IVHHHMMMHMMMMI
+	        :."VIIHHMMA:.  .   .   .:  .:.. . .:.II:I:AMMMMMMHMMMMMI
+	        :..VIHIHMMMI...::.,:.,:!"I:!"I!"I!"V:AI:VAMMMMMMHMMMMMM'
+	        ':.:HIHIMHHA:"!!"I.:AXXXVVXXXXXXXA:."HPHIMMMMHHMHMMMMMV
+	          V:H:I:MA:W'I :AXXXIXII:IIIISSSSSSXXA.I.VMMMHMHMMMMMM
+	            'I::IVA ASSSSXSSSSBBSBMBSSSSSSBBMMMBS.VVMMHIMM'"'
+	             I:: VPAIMSSSSSSSSSBSSSMMBSSSBBMMMMXXI:MMHIMMI
+	            .I::. "H:XIIXBBMMMMMMMMMMMMMMMMMBXIXXMMPHIIMM'
+	            :::I.  ':XSSXXIIIIXSSBMBSSXXXIIIXXSMMAMI:.IMM
+	            :::I:.  .VSSSSSISISISSSBII:ISSSSBMMB:MI:..:MM
+	            ::.I:.  ':"SSSSSSSISISSXIIXSSSSBMMB:AHI:..MMM.
+	            ::.I:. . ..:"BBSSSSSSSSSSSSBBBMMMB:AHHI::.HMMI
+	            :..::.  . ..::":BBBBBSSBBBMMMB:MMMMHHII::IHHMI
+	            ':.I:... ....:IHHHHHMMMMMMMMMMMMMMMHHIIIIHMMV"
+	              "V:. ..:...:.IHHHMMMMMMMMMMMMMMMMHHHMHHMHP'
+	               ':. .:::.:.::III::IHHHHMMMMMHMHMMHHHHM"
+	                 "::....::.:::..:..::IIIIIHHHHMMMHHMV"
+	                   "::.::.. .. .  ...:::IIHHMMMMHMV"
+	                     "V::... . .I::IHHMMV"'
+	                       '"VHVHHHAHHHHMMV:"'
+
+	                       \n
+		"""
 		dead("The old man sucks out your very soul.")
 
 
@@ -481,7 +584,8 @@ def back_outside():
 
 def car():
 	print "Reaching the car and open it up with the keys. Start the engine and get drive out through the gap in the trees.\n"
-	print car_text2
+	print """Blah Blah Blah reach the edge of the wood where what appears to be a dusty track emerges in sight, just as you reach it a blinding light fills looms forth
+	blinding you. BLAH BLAH BLAH - wake up and all is revealed."""
 
 
 start()
---
+++ house_of_horrors/text.py
@@ -0,0 +1,188 @@
+#place holder for al of the required text
+
+opening_line = "Let's start with a few questions."
+
+start_text = """A test, a test for the soul while the mind is occupied. Save your soul and your mind may just flicker again, consciousness is a cruel concept. You need a foundation, 
+	a corner stone in which to base your percpetions on, don't keep your eyeys shut for too long, when you open them things may seem too real. I understand, you're confused. That's 
+	perfectly reasonable, but you you need to take action now if you wish to become lucid again. Now wake up, but not completely, you can only save yourself if your self thinks it's awake.
+	\nI can be of some assistance later on, things may become clearer, but for now wake up!\n"""
+
+wake_text = """As his eyes creaked opened he became aware of the throbbing pain that was present in the back of his head. Reaching back he brushed his fingers across an an egg sized 
+	lump that plateaued out from his head, it felt as though it was buffering against his brain as the vibrations could be felt throughout his body. As his surroundings started to 
+	become clear he realised he was lying on his side surrounded by a quilt of fallen autumn leaves, as he shifted his weight they crunched underneath him clunging to his clothes 
+	while he pushed himself up. Craning his neck he could see he was surrounded by woodland, it was extremely dark though 
+	so he could only see the first row of trees. The only light source available was the dull glow of the moon, it excentuated the silhouettes of the objects that surrounded him but 
+	didn't reveal any more detail. There was a break in the trees far off to his right and stationed in front of it was a small car, he couldn't make out much more than that due to 
+	the low visibility.\n Suddenly there was a bang behind him, he jolted round he found himself outside the front of an old wooden house, it was a 2 storey cabin. Agahst he scanned 
+	the front of the house erratically searching for the source of the noise. All was quiet though. Too quiet he thought, there were no animal noises, no trees rustling in the wind. 
+	In fact there was no wind at all. The back of his head began to throb again causing him to double over as he clutched at the swollen lump pertruding from it. ****WRITE MORE***
+	"""
+
+# HOUSE SECTION
+
+enter_house_text = """House is entered looks around a bit blah blah blah, on the floor he finds a note written on a post-it note. Not sure where the basement is just yet.  
+	Note reads that the car keys have been left in the safe in the basement, combination is 17XX - the last 2 digits are unintellgible.\n \"I need to find those last 2 digits he thought.\""""
+
+	#UPSTAIRS
+
+upstairs_text = """He walks up stairs and finds a long straight corridor with 3 rooms, 2 on the left 1 on the right. blah blah blah - does he enter one or head back downstairs?"""
+upstairs_hallway_choice_down = "He heads back down the stairs blah blah blah"
+
+	#room1
+
+room1_entry_text = "Blah Blah blah - He enters the first room"
+
+room1_initial_text = """Enters room 1 - turns out to be a bedroom, bedframe with bedside table, wardrobe. What should he inspect?
+	"""
+
+room1_bed_text = "Shit about what the bed looks like\n"
+
+	#room2
+
+room2_entry_text = "Blah Blah blah - He enters the second room"
+
+room2_initial_text = """Enters room 2 - it's the bathroom, blah blah blah. There's a shower, a broken toilet bowl and not much else. The light is flickering, the shower is dripping heavily
+	turn the shower handle to stop the dripping? 
+"""	
+
+room2_no_fix_shower = "The dripping wasn't so bad after all blah blah blah, back to the landing"
+
+	#room3
+
+room3_entry_text = "Blah Blah blah - He enters the third room"
+
+room3_initial_text = """ Third room is a study/office, it is baren except for a desk in front of a large bare window looking out onto the front yard of the house, 
+	the only light entering the room dully creeps in from the distant moon - blah blah blah. Walking over to the desk what should be explored? 
+	Look out the window, check out desk?
+
+	"""
+room3_window_text = """He looks out the window out across the yard...blah blah blah, he can see the outline of the car out towards the tree line reminding him
+		of what his goals are. He needs to get into that basement, where's the key though? And what's the rest of the combination for the safe? Should he check out
+		the desk now or head back out the room?"""
+
+room3_window_desk_text = """Turning his gaze away from the window he looks over the desk, pretty dark so he's not able to see much, tries to open the drawers he finds
+			but they are empty. There's an old lamp sitting on the top of the desk, it may still work should he turn it on?"""
+
+room3_desk_text = "Shit about the desk, ask whether to look out the window or leave."
+
+room3_desk_window_text = "He looks out the window out across the yard...blah blah blah then turns and leaves the room."
+
+room3_whisper_text = """He leaves the room, but hears a rustling from the corner of the room making he swing round in fright. There's nothing there so he leaves again, he then 
+			hears a whisper \"LAMP\""""
+room3_lamp_text = """He turns on the lamp, it fizzes and flickers as it attmepts to illuminate the desk, after a few seconds of flickering it goes full beam blinding him, he shields
+		his eyes momentarily. Slowly his eyes begin to adjust to the new lighting level and drops his arm from his eyes, he's looking at the window now but the new ligting means
+		it's reflected and so it is himself and the room that is looking back at him. He notices a post-it notepad in the refelction of the window resting on the table now. Picking it up 
+		he can see there are imprints on it...it must be the pad that the note he found was written on. Holding it under the lamp he can see it clearly, scanning it quickly he finds the the full
+		code - 1792!"""
+
+# DOWNSTAIRS
+
+downstairs_initial_text = """On the ground floor he blah blah blah blah - looks around a little there are 4 rooms, more description about what he sees, he needs to pick a room - A, 
+	B, C, or D Or should he go upstairs? Alternatively head back outside?"""
+
+	#rooma
+
+rooma_intial_text = """Enters Room A blah blah blah - it's the kitchen, looks around blah blah blah blah. Description of the kitchen and it's rustic demeanor. Get to the point 
+	where something enters and he has to reach for a knife but drops it down a crack and tries to reach for it in order to defend himself. The crack is slim, """
+
+rooma_too_fat_text = "You too fat! You dead! blah blah blah monster rips you to pieces"
+
+
+
+#BATTLE
+
+
+demon_text = """
+		                    ,-.                               
+       ___,---.__          /'|`\          __,---,___          
+    ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-.       
+  ,'        |           ~'\     /`~           |        `.      
+ /      ___//              `. ,'          ,  , \___      \    
+|    ,-'   `-.__   _         |        ,    __,-'   `-.    |    
+|   /          /\_  `   .    |    ,      _/\          \   |   
+\  |           \ \`-.___ \   |   / ___,-'/ /           |  /  
+ \  \           | `._   `\\  |  //'   _,' |           /  /      
+  `-.\         /'  _ `---'' , . ``---' _  `\         /,-'     
+     ``       /     \    ,='/ \`=.    /     \       ''          
+             |__   /|\_,--.,-.--,--._/|\   __|                  
+             /  `./  \\`\ |  |  | /,//' \,'  \                  
+            /   /     ||--+--|--+-/-|     \   \                 
+           |   |     /'\_\_\ | /_/_/`\     |   |                
+            \   \__, \_     `~'     _/ .__/   /            
+             `-._,-'   `-._______,-'   `-._,-'
+
+
+		"""
+
+# BASEMENT SCENE TEXT
+
+	skull = """
+		                          ...----....
+	                         ..-:"''         ''"-..
+	                      .-'                      '-.
+	                    .'              .     .       '.
+	                  .'   .          .    .      .    .''.
+	                .'  .    .       .   .   .     .   . ..:.
+	              .' .   . .  .       .   .   ..  .   . ....::.
+	             ..   .   .      .  .    .     .  ..  . ....:IA.
+	            .:  .   .    .    .  .  .    .. .  .. .. ....:IA.
+	           .: .   .   ..   .    .     . . .. . ... ....:.:VHA.
+	           '..  .  .. .   .       .  . .. . .. . .....:.::IHHB.
+	          .:. .  . .  . .   .  .  . . . ...:.:... .......:HIHMM.
+	         .:.... .   . ."::"'.. .   .  . .:.:.:II;,. .. ..:IHIMMA
+	         ':.:..  ..::IHHHHHI::. . .  ...:.::::.,,,. . ....VIMMHM
+	        .:::I. .AHHHHHHHHHHAI::. .:...,:IIHHHHHHMMMHHL:. . VMMMM
+	       .:.:V.:IVHHHHHHHMHMHHH::..:" .:HIHHHHHHHHHHHHHMHHA. .VMMM.
+	       :..V.:IVHHHHHMMHHHHHHHB... . .:VPHHMHHHMMHHHHHHHHHAI.:VMMI
+	       ::V..:VIHHHHHHMMMHHHHHH. .   .I":IIMHHMMHHHHHHHHHHHAPI:WMM
+	       ::". .:.HHHHHHHHMMHHHHHI.  . .:..I:MHMMHHHHHHHHHMHV:':H:WM
+	       :: . :.::IIHHHHHHMMHHHHV  .ABA.:.:IMHMHMMMHMHHHHV:'. .IHWW
+	       '.  ..:..:.:IHHHHHMMHV" .AVMHMA.:.'VHMMMMHHHHHV:' .  :IHWV
+	        :.  .:...:".:.:TPP"   .AVMMHMMA.:. "VMMHHHP.:... .. :IVAI
+	       .:.   '... .:"'   .   ..HMMMHMMMA::. ."VHHI:::....  .:IHW'
+	       ...  .  . ..:IIPPIH: ..HMMMI.MMMV:I:.  .:ILLH:.. ...:I:IM
+	     : .   .'"' .:.V". .. .  :HMMM:IMMMI::I. ..:HHIIPPHI::'.P:HM.
+	     :.  .  .  .. ..:.. .    :AMMM IMMMM..:...:IV":T::I::.".:IHIMA
+	     'V:.. .. . .. .  .  .   'VMMV..VMMV :....:V:.:..:....::IHHHMH
+	       "IHH:.II:.. .:. .  . . . " :HB"" . . ..PI:.::.:::..:IHHMMV"
+	        :IP""HHII:.  .  .    . . .'V:. . . ..:IH:.:.::IHIHHMMMMM"
+	        :V:. VIMA:I..  .     .  . .. . .  .:.I:I:..:IHHHHMMHHMMM
+	        :"VI:.VWMA::. .:      .   .. .:. ..:.I::.:IVHHHMMMHMMMMI
+	        :."VIIHHMMA:.  .   .   .:  .:.. . .:.II:I:AMMMMMMHMMMMMI
+	        :..VIHIHMMMI...::.,:.,:!"I:!"I!"I!"V:AI:VAMMMMMMHMMMMMM'
+	        ':.:HIHIMHHA:"!!"I.:AXXXVVXXXXXXXA:."HPHIMMMMHHMHMMMMMV
+	          V:H:I:MA:W'I :AXXXIXII:IIIISSSSSSXXA.I.VMMMHMHMMMMMM
+	            'I::IVA ASSSSXSSSSBBSBMBSSSSSSBBMMMBS.VVMMHIMM'"'
+	             I:: VPAIMSSSSSSSSSBSSSMMBSSSBBMMMMXXI:MMHIMMI
+	            .I::. "H:XIIXBBMMMMMMMMMMMMMMMMMBXIXXMMPHIIMM'
+	            :::I.  ':XSSXXIIIIXSSBMBSSXXXIIIXXSMMAMI:.IMM
+	            :::I:.  .VSSSSSISISISSSBII:ISSSSBMMB:MI:..:MM
+	            ::.I:.  ':"SSSSSSSISISSXIIXSSSSBMMB:AHI:..MMM.
+	            ::.I:. . ..:"BBSSSSSSSSSSSSBBBMMMB:AHHI::.HMMI
+	            :..::.  . ..::":BBBBBSSBBBMMMB:MMMMHHII::IHHMI
+	            ':.I:... ....:IHHHHHMMMMMMMMMMMMMMMHHIIIIHMMV"
+	              "V:. ..:...:.IHHHMMMMMMMMMMMMMMMMHHHMHHMHP'
+	               ':. .:::.:.::III::IHHHHMMMMMHMHMMHHHHM"
+	                 "::....::.:::..:..::IIIIIHHHHMMMHHMV"
+	                   "::.::.. .. .  ...:::IIHHMMMMHMV"
+	                     "V::... . .I::IHHMMV"'
+	                       '"VHVHHHAHHHHMMV:"'
+
+	                       \n
+		"""
+
+		poem = """
+	A flame bereft,
+		of control.
+	A bottomless depth,
+		without soul.
+	A hellish pit of,
+		sadistic comedy.
+	An inaugural piece of,
+		divine trinity.\n
+	"""
+
+# CAR SCENE
+
+car_text2 = """Blah Blah Blah reach the edge of the wood where what appears to be a dusty track emerges in sight, just as you reach it a blinding light fills looms forth
+	blinding you. BLAH BLAH BLAH - wake up and all is revealed."""
